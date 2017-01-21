696cc1fe8139c73994ec79ecfea49d9466085340
wojciech_kurleto <wojciech.kurleto@gmail.com>
2017-01-21 20:17:19 +0100

    Changes in Histogram Test

--- jis-documentation/src/test/java/pl/edu/uj/JImageStream/tests/filters/HistogramTest.java
+++ jis-documentation/src/test/java/pl/edu/uj/JImageStream/tests/filters/HistogramTest.java
@@ -1,10 +1,15 @@
 package pl.edu.uj.JImageStream.tests.filters;
 
+import javax.imageio.ImageIO;
+
 import org.junit.Test;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 import pl.edu.uj.JImageStream.collectors.BufferedImageCollector;
 import pl.edu.uj.JImageStream.collectors.FileCollector;
-import pl.edu.uj.JImageStream.filters.Histogram;
+import pl.edu.uj.JImageStream.filters.statistical.Histogram;
 import pl.edu.uj.JImageStream.model.ColorChannel;
 import pl.edu.uj.JImageStream.model.StreamableImage;
 import pl.edu.uj.JImageStream.tests.AbstractBaseTest;
@@ -14,11 +19,17 @@ public class HistogramTest extends AbstractBaseTest{
     public void histogramFilterTest() {
         BufferedImage hBase = new BufferedImage(512, 180, BufferedImage.TYPE_INT_ARGB);
         BufferedImage lenaImage = streamableImage.stream().collect(new BufferedImageCollector());
-        
+        streamableImage = new StreamableImage(hBase);
+
         // tag::histogramFilter[]
-        new StreamableImage(hBase).stream()
+        BufferedImage bufferedImage = streamableImage.stream()
                 .apply(new Histogram(lenaImage, ColorChannel.RED, ColorChannel.GREEN, ColorChannel.BLUE))
-                .collect(new FileCollector("png", "target/docs/images/RGBAhistogram.png"));
+                .collect(new BufferedImageCollector());
         // end::histogramFilter[]
+        try {
+            ImageIO.write(bufferedImage, "png", new File("target/docs/images/RGBAhistogram.png"));
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
