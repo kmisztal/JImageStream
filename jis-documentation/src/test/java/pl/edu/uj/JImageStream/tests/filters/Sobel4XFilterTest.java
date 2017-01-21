3789e6f53291a796aafd731d10f8acfe82a0d784
Aga Rakowska <ag.rakowska@gmail.com>
2017-01-21 19:24:53 +0100

    REFACTOR: Filters doc restored & code tags to Sobel4XFilterTest added

--- jis-documentation/src/test/java/pl/edu/uj/JImageStream/tests/filters/Sobel4XFilterTest.java
+++ jis-documentation/src/test/java/pl/edu/uj/JImageStream/tests/filters/Sobel4XFilterTest.java
@@ -19,10 +19,12 @@ public class Sobel4XFilterTest extends AbstractBaseTest {
 
     @Test
     public void sobelFilterTest() {
+        // tag::sobelXFilter[]
         BufferedImage bufferedImageX = streamableImage.parallelStream()
                 .apply(new GrayScaleFilter())
                 .apply(new SobelXFilter())
                 .collect(new BufferedImageCollector());
+        // end::sobelXFilter[]
 
         try {
             ImageIO.write(bufferedImageX, "png", new File("target/docs/images/SobelX.png"));
@@ -30,42 +32,45 @@ public class Sobel4XFilterTest extends AbstractBaseTest {
             e.printStackTrace();
         }
 
+        // tag::sobelYFilter[]
         BufferedImage bufferedImageY = streamableImage.parallelStream()
                 .apply(new GrayScaleFilter())
                 .apply(new SobelYFilter())
                 .collect(new BufferedImageCollector());
+        // end::sobelYFilter[]
 
         try {
             ImageIO.write(bufferedImageY, "png", new File("target/docs/images/SobelY.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
+        // tag::sobel45Filter[]
         BufferedImage bufferedImage45 = streamableImage.parallelStream()
                 .apply(new GrayScaleFilter())
                 .apply(new Sobel45Filter())
                 .collect(new BufferedImageCollector());
-
+        // end::sobel45Filter[]
         try {
             ImageIO.write(bufferedImage45, "png", new File("target/docs/images/Sobel45.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        // tag::sobel135Filter[]
         BufferedImage bufferedImage135 = streamableImage.parallelStream()
                 .apply(new GrayScaleFilter())
                 .apply(new Sobel135Filter())
                 .collect(new BufferedImageCollector());
-
+        // end::sobel135Filter[]
         try {
             ImageIO.write(bufferedImage135, "png", new File("target/docs/images/Sobel135.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        // tag::edgeDetectionSobel4XFilter[]
         BufferedImage bufferedImage = streamableImage.parallelStream()
                 .apply(new EdgeDetection4XFilter(bufferedImageX, bufferedImageY,bufferedImage45,bufferedImage135))
                 .collect(new BufferedImageCollector());
-
+        // end::edgeDetectionSobel4XFilter[]
         try {
             ImageIO.write(bufferedImage, "png", new File("target/docs/images/Sobel4X.png"));
         } catch (IOException e) {
