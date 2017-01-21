c5c71709ce4ec4fa4e863dcd77adadef8ff12863
AdrianLato <adrian.lato@student.uj.edu.pl>
2017-01-21 17:47:42 +0100

    AddImageFilterTest

---
+++ jis-documentation/src/test/java/pl/edu/uj/JImageStream/tests/filters/AddImageFilterTest.java
@@ -0,0 +1,38 @@
+package pl.edu.uj.JImageStream.tests.filters;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import org.junit.Test;
+import pl.edu.uj.JImageStream.collectors.BufferedImageCollector;
+import pl.edu.uj.JImageStream.filters.arithmetic.AddImageFilter;
+import pl.edu.uj.JImageStream.tests.AbstractBaseTest;
+
+public class AddImageFilterTest extends AbstractBaseTest {
+    @Test
+    public void addImageFilterTest() {
+
+        BufferedImage addBufferedImage = null;
+        try {
+            addBufferedImage = ImageIO.read(new File("target/docs/images/lena.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // tag::AddImageFilter[]
+        BufferedImage bufferedImage = streamableImage.stream()
+                .apply(new AddImageFilter(addBufferedImage))
+                .collect(new BufferedImageCollector());
+        // end::AddImageFilter[]
+
+        try {
+            ImageIO.write(bufferedImage, "png", new File("target/docs/images/AddImageFilter.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+
