1a0cf3fff613ae34e73bbe71340176b7a81559b4
matmarchew <matmarchew@gmail.com>
2017-01-26 17:22:18 +0100

    Add rotate function and update documentation

---
+++ jis-documentation/src/test/java/pl/edu/uj/JImageStream/tests/ResizeTest.java
@@ -0,0 +1,27 @@
+package pl.edu.uj.JImageStream.tests;
+
+import org.junit.Test;
+import pl.edu.uj.JImageStream.collectors.BufferedImageCollector;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ResizeTest extends AbstractBaseTest {
+
+    @Test
+    public void ResizeTest() {
+        // tag::resize[]
+        BufferedImage bufferedImage = streamableImage.stream()
+                .resize(600,600)
+                .collect(new BufferedImageCollector());
+        // end::resize[]
+
+        try {
+            ImageIO.write(bufferedImage, "png", new File("target/docs/images/Resize.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
