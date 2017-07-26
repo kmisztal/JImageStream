71ce91532a079cac3d64a5fac22d49863ef56a33
Jakub Grześka <k.grzeska@gmail.com>
2017-07-26 14:45:02 +0200

    added hit or miss filter

---
+++ jis-plugins/src/main/java/pl/edu/uj/JImageStream/filters/morphology/HitOrMissFilter.java
@@ -0,0 +1,54 @@
+package pl.edu.uj.JImageStream.filters.morphology;
+
+import com.sun.javaws.exceptions.InvalidArgumentException;
+import pl.edu.uj.JImageStream.api.core.Filter;
+import pl.edu.uj.JImageStream.model.Pixel;
+
+public class HitOrMissFilter extends Filter {
+
+    private int[][] hitOrMissMatrix;
+
+    public HitOrMissFilter(int[][] hitOrMissMatrix) throws InvalidArgumentException {
+        if (hitOrMissMatrix == null) {
+            throw new RuntimeException("Hit or miss matrix can't be null");
+        }
+        this.hitOrMissMatrix = hitOrMissMatrix;
+    }
+
+    @Override
+    public void apply(int x, int y) {
+
+        int widthMiddle = hitOrMissMatrix[0].length / 2 + 1;
+        int heightMiddle = hitOrMissMatrix.length / 2 + 1;
+
+        boolean hit = true;
+        for (int i = x - widthMiddle; i <= x + widthMiddle; ++i) {
+            for (int j = y - heightMiddle; j <= y + heightMiddle; ++j) {
+                try {
+                    int value = getPixel(i, j).getRed();
+                    switch (hitOrMissMatrix[j - (y - heightMiddle)][i - (x - widthMiddle)]) {
+                        case -1:
+                            if (value == 255) {
+                                hit = false;
+                            }
+                            break;
+                        case 1:
+                            if (value == 0) {
+                                hit = false;
+                            }
+                        default:
+                    }
+                } catch (ArrayIndexOutOfBoundsException ex) {
+                    // expected on corners
+                }
+            }
+        }
+
+        if (hit) {
+            setPixel(x, y, new Pixel(255, 255, 255, 255));
+        } else {
+            setPixel(x, y, new Pixel(0, 0, 0, 255));
+        }
+
+    }
+}
